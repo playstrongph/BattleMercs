d966158a6a6300cb0b16a0c6c722827ce5cf5669
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-07-29 15:28:02 +0800

    HeroSkillsVisual

--- BattleMercs/Assets/1Scripts/Visual/HeroSkillsVisual.cs
+++ BattleMercs/Assets/1Scripts/Visual/HeroSkillsVisual.cs
@@ -1,24 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class HeroSkillsVisual : MonoBehaviour
+namespace _1Scripts.Visual
 {
-   #region VARIABLES
+   public class HeroSkillsVisual : MonoBehaviour, IHeroSkillsVisual
+   {
+      #region VARIABLES
 
-        
+      [Header("Components")] 
+      
 
-   #endregion
-        
-   #region PROPERTIES
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkillPanelVisual))] private Object threeSkillPanelVisual = null;
+      
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkillPanelVisual))] private Object fourSkillPanelVisual = null;
+      [SerializeField] private Canvas canvas = null;
 
-        
+      [Header("Inspector References")]
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IBattleSceneManagerVisual))] private Object battleSceneManagerVisual = null;
+      
+      
 
-   #endregion
-        
-   #region METHODS
+      #endregion
 
-        
+      #region PROPERTIES
+      private Canvas Canvas => canvas;
+      public IBattleSceneManagerVisual BattleSceneManagerVisual => battleSceneManagerVisual as IBattleSceneManagerVisual;
 
-   #endregion
+      public ISkillPanelVisual ThreeSkillPanelVisual => threeSkillPanelVisual as ISkillPanelVisual;
+      
+      public ISkillPanelVisual FourSkillPanelVisual => fourSkillPanelVisual as ISkillPanelVisual;
+
+
+      #endregion
+
+      #region METHODS
+
+      public void ShowHeroSkillsVisual()
+      {
+         Canvas.enabled = true;
+      }
+      
+      public void HideHeroSkillsVisual()
+      {
+         Canvas.enabled = false;
+      }
+      
+      
+      
+
+
+      #endregion
+   }
 }
---
+++ BattleMercs/Assets/1Scripts/Visual/IHeroSkillsVisual.cs
@@ -0,0 +1,11 @@
+﻿namespace _1Scripts.Visual
+{
+    public interface IHeroSkillsVisual
+    {
+        ISkillPanelVisual ThreeSkillPanelVisual { get; }
+        ISkillPanelVisual FourSkillPanelVisual { get; }
+        IBattleSceneManagerVisual BattleSceneManagerVisual { get; }
+        void ShowHeroSkillsVisual();
+        void HideHeroSkillsVisual();
+    }
+}
