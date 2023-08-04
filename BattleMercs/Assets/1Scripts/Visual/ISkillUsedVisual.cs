cf96996e49ffffa74a094981b5960d6deab241d2
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-08-04 17:40:31 +0800

    SkillUsedVisual

---
+++ BattleMercs/Assets/1Scripts/Visual/ISkillUsedVisual.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine.UI;
+
+namespace _1Scripts.Visual
+{
+    public interface ISkillUsedVisual
+    {
+        ISkillPreviewFrames SkillPreviewFrames { get; }
+        IHeroFramesVisual HeroFramesVisual { get; }
+        Image SkillGraphic { get; }
+        Image CooldownImage { get; }
+        Image SpeedImage { get; }
+        Image ArrowGlowImage { get; }
+        Image ArrowImage { get; }
+        Image HeroImage { get; }
+        TextMeshProUGUI NameText { get; }
+        TextMeshProUGUI ElementText { get; }
+        TextMeshProUGUI DescriptionText { get; }
+        TextMeshProUGUI CooldownText { get; }
+        TextMeshProUGUI SpeedText { get; }
+    }
+}
---
+++ BattleMercs/Assets/1Scripts/Visual/SkillUsedVisual.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _1Scripts.Visual
+{
+   public class SkillUsedVisual : MonoBehaviour, ISkillUsedVisual
+   {
+      #region VARIABLES
+
+      //TODO: Inspector Reference for UsedSkillPreviewVisual
+
+      [Header("Components")] 
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkillPreviewFrames))] private Object skillPreviewFrames = null;
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IHeroFramesVisual))] private Object heroFramesVisual = null;
+
+      [Header("Images")] 
+      [SerializeField] private Image skillGraphic = null;
+      [SerializeField] private Image cooldownImage = null;
+      [SerializeField] private Image speedImage = null;
+      [SerializeField] private Image arrowGlowImage = null;
+      [SerializeField] private Image arrowImage = null;
+      [SerializeField] private Image heroImage = null;
+
+      [Header("Texts")] 
+      [SerializeField] private TextMeshProUGUI nameText = null;
+      [SerializeField] private TextMeshProUGUI elementText = null;
+      [SerializeField] private TextMeshProUGUI descriptionText = null;
+      [SerializeField] private TextMeshProUGUI cooldownText = null;
+      [SerializeField] private TextMeshProUGUI speedText = null;
+
+      #endregion
+
+      #region PROPERTIES
+
+      //Components
+      public ISkillPreviewFrames SkillPreviewFrames => skillPreviewFrames as ISkillPreviewFrames;
+      public IHeroFramesVisual HeroFramesVisual => heroFramesVisual as IHeroFramesVisual;
+      
+      //Images
+      public Image SkillGraphic => skillGraphic;
+      public Image CooldownImage => cooldownImage;
+      public Image SpeedImage => speedImage;
+      public Image ArrowGlowImage => arrowGlowImage;
+      public Image ArrowImage => arrowImage;
+      public Image HeroImage => heroImage;
+      
+      //Texts
+      public TextMeshProUGUI NameText => nameText;
+      public TextMeshProUGUI ElementText => elementText;
+      public TextMeshProUGUI DescriptionText => descriptionText;
+      public TextMeshProUGUI CooldownText => cooldownText;
+      public TextMeshProUGUI SpeedText => speedText;
+
+      #endregion
+
+      #region METHODS
+
+
+
+      #endregion
+   }
+}
