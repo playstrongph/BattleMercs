60dd46dfafbef86a1fe0bd8d44eccfdddda2ef6d
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-08-06 13:14:40 +0800

    Skill Targeting Visual

---
+++ BattleMercs/Assets/1Scripts/Visual/SkillTargetingVisual.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _1Scripts.Visual
+{
+   public class SkillTargetingVisual : MonoBehaviour
+   {
+      #region VARIABLES
+
+      [Header("Transforms")] 
+      [SerializeField] private Transform crossHair = null;
+      [SerializeField] private Transform arrow = null;
+      [SerializeField] private List<Transform> nodes = new List<Transform>();
+    
+      
+      [Header("Images")] 
+      [SerializeField] private Image crossHairImage = null;
+      [SerializeField] private Image arrowImage = null;
+      [SerializeField] private List<Image> nodesImage = new List<Image>();
+    
+      
+     
+      #endregion
+
+      #region PROPERTIES
+
+      public Transform CrossHair => crossHair;
+      public Transform Arrow => arrow;
+      private List<Transform> Nodes => nodes;
+   
+      
+      public Image CrossHairImage => crossHairImage;
+      public Image ArrowImage => arrowImage;
+      private List<Image> NodesImage => nodesImage;
+     
+      
+   
+      
+
+      #endregion
+
+      #region METHODS
+
+
+
+      #endregion
+   }
+}
