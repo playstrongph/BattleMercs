eab14d725288032cc9ade9cf623e08b96da8f11c
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-08-08 15:17:11 +0800

    DraggableWIP

---
+++ BattleMercs/Assets/1Scripts/Visual/Draggable.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace _1Scripts.Visual
+{
+   public class Draggable : MonoBehaviour
+   {
+      #region VARIABLES
+
+      [Header("Inspector References")]
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkillTargetingVisual))] private Object skillTargetingVisual = null;
+      [SerializeField]  private Camera cameraUsed = null;
+
+      [Header("Components")] 
+      [SerializeField] private BoxCollider arrowCollider = null;
+     
+      
+      //private variables
+      private readonly Vector3 _pointerDisplacement = Vector3.zero;
+      private readonly float _zDisplacement = 0f;
+     
+      
+      private delegate void ShowSkillTargeting();
+      private ShowSkillTargeting _skillTargeting;
+      
+      #endregion
+
+      #region PROPERTIES
+      
+      public BoxCollider ArrowCollider => arrowCollider;
+      
+      private ISkillTargetingVisual SkillTargetingVisual => skillTargetingVisual as ISkillTargetingVisual;
+      private Camera CameraUsed => cameraUsed;
+      
+      #endregion
+
+      #region METHODS
+      
+      private void Awake()
+      {
+         _skillTargeting = NoAction;
+      }
+
+      private void Update()
+      {
+         _skillTargeting();
+      }
+      
+      
+      /// <summary>
+      /// Convert the mouse coordinates into world coordinates
+      /// </summary>
+      /// <returns></returns>
+      private Vector3 MouseInWorldCoords()
+      {
+         var screenMousePos = Input.mousePosition;
+         screenMousePos.z = _zDisplacement;
+         return CameraUsed.ScreenToWorldPoint(screenMousePos);
+      }
+      
+      /// <summary>
+      /// Enables draggable script
+      /// specifically, the update method
+      /// </summary>
+      public void EnableDraggable()
+      {
+         this.enabled = true;
+         _skillTargeting = UpdateAction;
+      }
+      
+      /// <summary>
+      /// Disables the draggable script
+      /// prevents the Update method from running continuously
+      /// </summary>
+      public void DisableDraggable()
+      {
+         _skillTargeting = NoAction;
+         this.enabled = false;
+      }
+      
+      private void UpdateAction()
+      {
+         var mousePos = MouseInWorldCoords();    
+         var thisTransform = this.transform;
+            
+         thisTransform.position = new Vector3(mousePos.x - _pointerDisplacement.x, mousePos.y - _pointerDisplacement.y, thisTransform.position.z);
+         
+         //TODO:
+         //SkillTargetingVisual.ShowArrowLineAndCrossHair.TurnOn();
+      }
+      
+      
+      
+      
+      
+      
+      
+      /// <summary>
+      /// Dummy method used by _skillTargeting in order not to needlessly run logic in Update
+      /// </summary>
+      private void NoAction() {}
+      
+      
+
+      #endregion
+   }
+}
---
+++ BattleMercs/Assets/1Scripts/Visual/ScenePrefabsVisual.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace _1Scripts.Visual
+{
+   public class ScenePrefabsVisual : MonoBehaviour
+   {
+      #region VARIABLES
+
+        
+
+      #endregion
+        
+      #region PROPERTIES
+
+        
+
+      #endregion
+        
+      #region METHODS
+
+      private void Awake()
+      {
+         this.gameObject.SetActive(false);
+      }
+
+      #endregion
+   }
+}
