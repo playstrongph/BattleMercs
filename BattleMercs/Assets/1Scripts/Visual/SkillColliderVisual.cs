e4586a852d1aecf989bb87989ae77d26d4aa4832
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-08-09 14:05:30 +0800

    SkillsVisual
    Collider Position

---
+++ BattleMercs/Assets/1Scripts/Visual/SkillColliderVisual.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace _1Scripts.Visual
+{
+   public class SkillColliderVisual : MonoBehaviour
+   {
+      #region VARIABLES
+
+        
+
+      #endregion
+        
+      #region PROPERTIES
+
+        
+
+      #endregion
+        
+      #region METHODS
+
+      private void OnMouseDown()
+      {
+         Debug.Log("Skill Collider Visual On Mouse Down ");
+      }
+
+      #endregion
+   }
+}
