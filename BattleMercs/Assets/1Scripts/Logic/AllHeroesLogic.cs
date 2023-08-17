79368584036bfa100bc1362d73cd2cd9573e72c5
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-08-17 16:11:20 +0800

    All Heroes Logic

---
+++ BattleMercs/Assets/1Scripts/Logic/AllHeroesLogic.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace _1Scripts.Logic
+{
+   
+   
+
+   public class AllHeroesLogic : MonoBehaviour
+   {
+      #region VARIABLES
+
+      [SerializeField] private List<HeroesStruct> allHeroes = new List<HeroesStruct>();
+      
+      #endregion
+
+      
+
+      #region STRUCTS
+      
+      /// <summary>
+      /// This is effectively the heroLogic Information
+      /// </summary>
+
+      [Serializable]
+      public struct HeroesStruct 
+      {
+         #region StructVariables
+         
+         #pragma warning disable 0649
+         
+         //This is for changing the list element name only
+         [SerializeField] private string listLabel;
+         
+         [SerializeField] private HeroInformation heroInformation;
+
+         #pragma warning restore 0649 
+
+         public HeroInformation HeroInformation => heroInformation;
+
+         #endregion
+      }
+      
+      [Serializable]
+      public struct HeroInformation 
+      {
+         #region StructVariables
+         
+         #pragma warning disable 0649
+         
+         //[Header("Hero Information")]
+         [SerializeField] private string heroName;
+         [SerializeField] private int heroLevel;
+         [SerializeField] private int heroStars;
+         [SerializeField] private int heroCp;
+
+         #pragma warning restore 0649 
+         #endregion
+
+         #region StructProperties
+
+         public string HeroName => heroName;
+         public int HeroLevel => heroLevel;
+         public int HeroStars => heroStars;
+         public int HeroCp => heroCp;
+
+         #endregion
+      }
+
+      #endregion
+        
+      #region PROPERTIES
+      
+      public List<HeroesStruct> AllHeroes => allHeroes;
+
+      #endregion
+
+      #region METHODS
+
+      private void Test()
+      {
+         var cP = AllHeroes[0];
+      }
+
+
+      #endregion
+   }
+}
---
+++ BattleMercs/Assets/1Scripts/Logic/SOLogicScripts/HeroClassAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _1Scripts.Logic.SOLogicScripts
+{
+   /// <summary>
+   /// Fighter Green
+   /// Caster Blue
+   /// Defender Red
+   /// </summary>
+   public class HeroClassAsset : ScriptableObject
+   {
+      #region METHODS
+
+        
+
+      #endregion
+   }
+}
